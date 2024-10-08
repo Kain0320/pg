47225e2e83670d7b239db2e497cf9d4ed7ab0a18
Kain0320 <vip.turkanov21@gmail.com>
2024-10-08 15:48:23 +0000

    sudy_nebo_lichy

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print ("Hello World") 
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print("Číslo {cislo} je sudé")
+    else:
+        print("Číslo {cislo} je liché")
+    cislo = 5
+
+ #Voláni funkce
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(100000)
