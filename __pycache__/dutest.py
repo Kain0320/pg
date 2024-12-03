e85fbb13d69f4408aaa27bde1e1152ebcd78b993
Kain0320 <vip.turkanov21@gmail.com>
2024-12-03 19:51:01 +0000

    ok

---
+++ __pycache__/dutest.py
@@ -0,0 +1,9 @@
+from eight import bin_to_dec , test_funkce
+
+def test_bin_to_dec():
+    assert bin_to_dec("0") == 0
+    assert bin_to_dec(1) == 1
+    assert bin_to_dec("100") == 4
+    assert bin_to_dec(101) == 5
+    assert bin_to_dec("010101") == 21
+    assert bin_to_dec(10000000) == 128
